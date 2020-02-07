ad581718ece74e4683c1bd39931467eb47b797be
Peter allain <pballain910@gmail.com>
2020-02-07 14:34:29 -0500

    createContact post

--- controllers/web.js
+++ controllers/web.js
@@ -1,21 +1,31 @@
-const models = require('../models')
+const axios = require('axios')
 
-async function postContact(request, response) {
-    let { name, doing_business_as, type, date_of_birth, ssn, fein_tax, legal_entity_type, date_business_started,
-        phone, email, address, cognito_username, is_god, roles, username, confirmation_email } = request.body
-    if (!name || !doing_business_as || !type || !date_of_birth || !ssn || !fein_tax || !legal_entity_type || !date_business_started || !phone
-        || !email || !address || !cognito_username || !is_god || !roles || !username || !confirmation_email) {
-        response.status(400).send('The following fields are required: name, doing_business_as, type, date_of_birth, ssn, fein_tax, legal_entity_type, date_business_started, phone, email, address, cognito_username, is_god, roles, username, confirmation_email')
-    }
-
-
-    const newContact = await models.Contacts.create({
-        name, doing_business_as, type, date_of_birth, ssn, fein_tax, legal_entity_type, date_business_started,
-        phone, email, address, cognito_username, is_god, roles, username, confirmation_email
-    })
-
-    response.sendstatus(201).send(newContact)
+async function getIndex(request, response) {
+    return response.render('index')
 }
 
+async function getEmbroker(request, response) {
+    return response.render('embroker')
+}
+async function createContact(request, response) {
+    let { name, doing_business_as, date_of_birth, ssn, fein_tax, legal_entity_type, date_business_started, phone, email, addresses: { address_line1, address_city, address_state, address_zip, address_country, address_city_other, address_country_other, address_state_other }, cognito_username, is_god, roles, username, confirmation_email
+    } = request.body
+    let newContact = await axios.post('https://embroker-test.britecorepro.com/api/v2/contacts/new_contact', {
+        auth: 'api_key: 8d368a8d5ef8384610efaee8c320d3e0aebf052377804f8e90e822e7'
+    })
 
-module.exports = postContact
+    if (!name || !doing_business_as || !type || !date_of_birth || !ssn || !fein_tax || !legal_entity_type || !date_business_started || !cognito_username || !is_god || !roles || !username || !confirmation_email
+    )
+
+        return updatedProject
+            ? response.render('index', { updatedProject })
+            : response.sendStatus(404)
+
+}
+
+async function getProjectReport(request, response) {
+    return response.render('getProject')
+
+}
+
+module.exports = { getEmbroker, /*getProjectReport,*/ getIndex }
--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 
 const bodyParser = require('body-parser')
 const express = require('express')
-const { postContact } = require('./controllers/web')
+const { postContact, getEmbroker, getIndex } = require('./controllers/web')
+const path = require('path')
 
 const app = express()
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'pug')
 
-
+app.get('/', getIndex)
+app.get('/embroker', getEmbroker)
 
 const server = app.listen(1337, () => { console.log('listening on port 1337') })
 
