14758befd6e31db31393722d0baddb0ec6ce77b3
Peter allain <pballain910@gmail.com>
2020-03-11 16:41:24 -0400

    Updated contacts module

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const bodyParser = require('body-parser')
 const express = require('express')
 const { getEmbroker, getIndex, assignRole } = require('./controllers/web')
+const { getContactByParams } = require('./controllers/contactsModule')
 const path = require('path')
 
 const app = express()
@@ -10,9 +11,9 @@ app.set('view engine', 'pug')
 app.use(express.static(__dirname + 'static'))
 app.use(bodyParser.json())
 app.get('/', getIndex)
-app.get('/embroker', getEmbroker)
+app.get('/contact', getContactByParams)
 app.post('/role', assignRole)
-app.post('/newcontact', async (request, response) => {
+/*app.post('/newcontact', async (request, response) => {
     const { } = request.body
 
     const newContact = await axios.post('https://embroker-test.britecorepro.com/api/v2/contacts/add_contact_to_role', {
@@ -22,7 +23,7 @@ app.post('/newcontact', async (request, response) => {
 
     })
     response.sendStatus(201).send(newContact)
-})
+})*/
 
 const server = app.listen(1337, () => { console.log('listening on port 1337') })
 
